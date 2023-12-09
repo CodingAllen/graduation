c6546d46dc67376693b28e26db0b5627c5817c64
CodingAllen <wencheng198949@gmail.com>
2023-12-09 23:02:09 +0900

    12/9/1

--- helpers/config.php
+++ helpers/config.php
@@ -1,4 +1,9 @@
 <?php
-define('DSN', 'sqlsrv:server=10.32.97.1\sotsu;database=22jn01_J');
-define('DB_USER', '22jn01_J');
-define('DB_PASSWORD', '22jn01_J');
+//define('DSN', 'sqlsrv:server=10.32.97.1\sotsu;database=22jn01_J');
+//define('DB_USER', '22jn01_J');
+//define('DB_PASSWORD', '22jn01_J');
+
+
+define('DSN', getenv('MY_DB_DSN') ?: 'sqlsrv:server=10.32.97.1\sotsu;database=22jn01_J');
+define('DB_USER', getenv('MY_DB_USER') ?: '22jn01_J');
+define('DB_PASSWORD', getenv('MY_DB_PASSWORD') ?: '22jn01_J');
